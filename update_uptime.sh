47bd123274580f03d53adf035ee5608ea12de92c
makise-homura <akemi_homura@kurisa.ch>
2023-02-21 22:44:19 +0300

    Fix minor inconsistencies in update scripts

--- update_uptime.sh
+++ update_uptime.sh
@@ -3,8 +3,8 @@
 URL="https://elbrus.kurisa.ch/"
 PKEYFILE="/etc/ssh/ssh_host_rsa_key"
 HASHTYPE="sha256"
-SERVER="yukari"
+SERVER="Yukari"
 
 UPTIME="`uptime | base64 -w0`"
 SIGNATURE="`echo -n "$UPTIME" | openssl dgst -sign $PKEYFILE | base64 -w0`"
-curl -X POST --data-urlencode uptime=$UPTIME --data-urlencode sig=$SIGNATURE --data-urlencode hashtype=$HASHTYPE $URL
+curl -X POST --data-urlencode uptime=$UPTIME --data-urlencode sig=$SIGNATURE --data-urlencode hashtype=$HASHTYPE --data-urlencode server=$SERVER $URL
